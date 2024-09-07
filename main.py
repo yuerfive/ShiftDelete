cb02d58a65328c6d359673084e48716127d10258
Anni <1582456060@qq.com>
2024-09-07 21:02:53 +0800

    master

---
+++ main.py
@@ -0,0 +1,15 @@
+# pyinstaller -F --uac-admin main.py --noconsole
+
+import subprocess
+from threading import Thread
+
+# 指定AutoHotkey解释器的路径和要运行的.ahk文件路径
+ahk_interpreter = r"C:\Program Files\AutoHotkey\v1.1.37.02\AutoHotkeyU64.exe"  # 根据你的安装路径调整
+ahk_script = r"GetFilePath.ahk"  # 替换为你的.ahk文件路径
+
+# 指定要运行的.exe文件路径
+exe_file = r"DeleteFile.exe"  # 替换为你的.exe文件路径
+
+Thread(target=subprocess.run, args=([ahk_interpreter, ahk_script],)).start()  # 启动一个线程运行.ahk文件
+
+Thread(target=subprocess.run, args=([exe_file],)).start()  # 启动一个线程运行.exe文件
