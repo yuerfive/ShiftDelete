5669f2b8306ff6a8cd7953e5b1f915409bc8a26b
Anni <1582456060@qq.com>
2024-09-08 00:11:48 +0800

    master

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess
 from threading import Thread
 
 # 指定AutoHotkey解释器的路径和要运行的.ahk文件路径
-ahk_interpreter = r"C:\Program Files\AutoHotkey\v1.1.37.02\AutoHotkeyU64.exe"  # 根据你的安装路径调整
+ahk_interpreter = r"D:\Program Files\AutoHotkey\AutoHotkeyU64.exe"  # 根据你的安装路径调整
 ahk_script = r"GetFilePath.ahk"  # 替换为你的.ahk文件路径
 
 # 指定要运行的.exe文件路径
